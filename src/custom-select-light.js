10f1a69275ed72abd2083def19c46369d81bf36a
Gionatan Lombardi <gionatan@vangogh-creative.it>
2016-06-15 12:57:29 +0200

    Added public exposed methods, remove, empty and add - plugin creation moved to a factory function

--- src/custom-select-light.js
+++ src/custom-select-light.js
@@ -14,67 +14,65 @@ if (typeof jQuery === 'undefined') {
 
   'use strict';
 
-  $.fn.customSelectLight = function( options ) {
-   
-    var self = this;
+  // Factory function
+  // Thanx to Inferpse @ http://stackoverflow.com/questions/12880256/jquery-plugin-creation-and-public-facing-methods#answer-12880432
+  function CustomSelectLight(item, options) {
 
-    // Extend the default options with those provided.
-    self.opts = $.extend( {}, $.fn.customSelectLight.defaults, options );
+      var self = this;
 
-    this.each( function () {
-
-      var $select = $(this);
-      var $container, $opener, $panel, $cstOption, optionsData, searchTimeout, searchString = "";
+      // Extend the default options with those provided.
+      self.options = $.extend( {}, $.fn.customSelectLight.defaults, options );
+      self.$select = $(item);
+      var searchTimeout, searchString = "";
 
       // Custom select: DOM element creation
       function customSelectCreation() {
         
-        optionsData = {
+        self.optionsData = {
           value: [],
           text: [],
           $items: []
         };
 
-        // Wrap the select in an outer div
-        $select.wrap('<div class="cstSelContainer ' + self.opts.containerClass + '"></div>')
-        $container = $select.closest('.cstSelContainer');
-
+        // Wrap the select in an outer div 
+        self.$select.wrap('<div class="cstSelContainer ' + self.options.containerClass + '"></div>')
+        self.$container = self.$select.closest('.cstSelContainer');
 
         var optionsMarkup = "";
 
         // Gets the values and the text of each option
-        $select.find('option').each(function(index, value) {
+        self.$select.find('option').each(function(index, value) {
           // Stores the data only if the option has a value
           if ($(this).val() !== "") {
-            optionsData.value.push($(this).val());
-            optionsData.text.push($(this).text());
+            self.optionsData.value.push($(this).val());
+            self.optionsData.text.push($(this).text());
           }
         })
 
         // Creates the options HTML markup
-        for (var i = 0; i < optionsData.value.length; i++) {
-          optionsMarkup += '<div class="cstSelOption ' + self.opts.optionClass + '" data-val="' + optionsData.value[i] + '"><span>' + optionsData.text[i] + '</span></div>';
+        for (var i = 0; i < self.optionsData.value.length; i++) {
+          optionsMarkup += '<div class="cstSelOption ' + self.options.optionClass + '" data-val="' + self.optionsData.value[i] + '"><span>' + self.optionsData.text[i] + '</span></div>';
         };
 
         // Creates the custom select HTML markup
-        var customSelectMarkup = '<div class="cstSelPanel ' + self.opts.panelClass + '">' + optionsMarkup + '</div>';
+        var customSelectMarkup = '<div class="cstSelPanel ' + self.options.panelClass + '">' + optionsMarkup + '</div>';
 
         // Writes the custom select opener before the select
-        $select.before('<span class="cstSelOpener ' + self.opts.openerClass + '" tabindex="0"><span></span></span>')
+        self.$select.before('<span class="cstSelOpener ' + self.options.openerClass + '" tabindex="0"><span></span></span>')
         
         // Writes the custom select HTML markup after the select
-        $select.after(customSelectMarkup);
+        self.$select.after(customSelectMarkup);
 
         // Stores new element in the main function vars
-        $panel = $(".cstSelPanel", $container);
-        $opener = $(".cstSelOpener", $container);
-        $cstOption = $(".cstSelOption", $container);
+        self.$panel = $(".cstSelPanel", self.$container);
+        self.$opener = $(".cstSelOpener", self.$container);
+        self.$cstOptions = $(".cstSelOption", self.$container);
         
         // Stores all the custom options in a global var
-        optionsData.$items = $cstOption;
+        self.optionsData.$items = self.$cstOptions;
 
         // If there is a preselected option updates the opener text
-        if ($select.find('option:selected').text() !== "") updateLabelText.call($select);
+        if (self.$select.find('option:selected').text() !== "") updateLabelText.call(self.$select);
       }
 
       // Open Panel
@@ -84,17 +82,17 @@ if (typeof jQuery === 'undefined') {
         $('.cstSelPanel').removeClass('is-open');
 
         // Opens only the clicked one
-        $opener.addClass('is-active');
-        $panel.addClass('is-open');
+        self.$opener.addClass('is-active');
+        self.$panel.addClass('is-open');
 
         // Sets the selected option
         setSelectedOption();
       }
 
       function closePanel() {
-        $opener.removeClass('is-active');
-        $panel.removeClass('is-open');
-        $cstOption.removeClass('has-focus');
+        self.$opener.removeClass('is-active');
+        self.$panel.removeClass('is-open');
+        self.$cstOptions.removeClass('has-focus');
       }
 
       function togglePanel(e) {
@@ -115,10 +113,10 @@ if (typeof jQuery === 'undefined') {
 
         // Set the value to the hidden select
         // And triggers the change event for label text update
-        $select.val(val).trigger("change");
+        self.$select.val(val).trigger("change");
 
         // Writes the value in the custom label
-        $opener.find('span').text(label);
+        self.$opener.find('span').text(label);
 
         // Closes the panel
         closePanel()
@@ -127,13 +125,13 @@ if (typeof jQuery === 'undefined') {
 
       // On real select changes, updates the text of the label
       function updateLabelText() {
-        $opener.find('span').text(this.find('option:selected').text());
+        self.$opener.find('span').text(this.find('option:selected').text());
       }
 
       function setSelectedOption() {
         var $elem, elemIndex
-        var value = $select.val();
-        $cstOption.each(function(index) {
+        var value = self.$select.val();
+        self.$cstOptions.each(function(index) {
           if ($(this).attr('data-val') === value) {
             $elem = $(this);
             elemIndex = index;
@@ -145,8 +143,8 @@ if (typeof jQuery === 'undefined') {
       // Ouside panel closing function
       function outsideClosePanel(e) {
         if (
-          !$opener.is(e.target) // the click is not on the opener
-          && $opener.has(e.target).length === 0 // the click is not on a descendant of the opener
+          !self.$opener.is(e.target) // the click is not on the opener
+          && self.$opener.has(e.target).length === 0 // the click is not on a descendant of the opener
           && isOpen()
         ) {
           closePanel();
@@ -175,8 +173,8 @@ if (typeof jQuery === 'undefined') {
           // sets the value from it
           if (e.keyCode == 13) {
             e.preventDefault();
-            if ( $panel.find('.has-focus').length === 1 ) {
-              setSelectValue.call($panel.find('.has-focus'))
+            if ( self.$panel.find('.has-focus').length === 1 ) {
+              setSelectValue.call(self.$panel.find('.has-focus'))
             }
           }
           
@@ -189,19 +187,19 @@ if (typeof jQuery === 'undefined') {
           // On "Arrow Down" focuses the next option or the first
           if (e.keyCode == 40) {
             e.preventDefault();
-            if ($panel.find('.has-focus').length === 1) {
-              var $toBeFocused = $panel.find('.has-focus').next()
+            if (self.$panel.find('.has-focus').length === 1) {
+              var $toBeFocused = self.$panel.find('.has-focus').next()
             } else {
-              var $toBeFocused = $cstOption.first();
+              var $toBeFocused = self.$cstOptions.first();
             }
-            setFocus($toBeFocused, $cstOption.index($toBeFocused), true, true);
+            setFocus($toBeFocused, self.$cstOptions.index($toBeFocused), true, true);
 
             // On "Arrow Up" focuses the prev option
           } else if (e.keyCode == 38) {
             e.preventDefault();
-            if ($panel.find('.has-focus').length === 1) {
-              var $toBeFocused = $panel.find('.has-focus').prev();
-              setFocus($toBeFocused, $cstOption.index($toBeFocused), true, true);
+            if (self.$panel.find('.has-focus').length === 1) {
+              var $toBeFocused = self.$panel.find('.has-focus').prev();
+              setFocus($toBeFocused, self.$cstOptions.index($toBeFocused), true, true);
             }
 
             // Otherwise start the search/autocomplete function
@@ -223,8 +221,8 @@ if (typeof jQuery === 'undefined') {
             e.preventDefault();
             ( function checkSetAndToggle (e) {
               if ( !isOpen() ) openPanel(e);
-              else if ( $panel.find('.has-focus').length === 1 ) {
-                setSelectValue.call($panel.find('.has-focus'))
+              else if ( self.$panel.find('.has-focus').length === 1 ) {
+                setSelectValue.call(self.$panel.find('.has-focus'))
               }
             })(e);
           }
@@ -234,7 +232,7 @@ if (typeof jQuery === 'undefined') {
       // Set the Focus state on the options
       function setFocus($elem, elemIndex, isMoving, isAnimated) {
         // Removes previous focused element
-        $cstOption.removeClass('has-focus');
+        self.$cstOptions.removeClass('has-focus');
 
         // Focuses the new element
         $elem.addClass('has-focus');
@@ -250,8 +248,8 @@ if (typeof jQuery === 'undefined') {
 
           // Scrolls to the selected element
           // if plugin scrollToSelectd option is setted to true
-          if (self.opts.scrollToSelected) {
-            $panel.stop().animate({
+          if (self.options.scrollToSelected) {
+            self.$panel.stop().animate({
               scrollTop: gotoPos
             }, duration);
           }
@@ -277,9 +275,9 @@ if (typeof jQuery === 'undefined') {
 
         // Loops through the options values
         // to find a match with the searched string
-        optionsData.text.every(function(value, index) {
+        self.optionsData.text.every(function(value, index) {
           if (value.match(searchRegExp)) {
-            $elem = $(optionsData.$items[index]);
+            $elem = $(self.optionsData.$items[index]);
             elemIndex = index;
             return false;
           }
@@ -307,30 +305,117 @@ if (typeof jQuery === 'undefined') {
 
       // Utility function to check if the panel is open
       function isOpen() {
-        if ( $panel.hasClass('is-open') ) return true;
+        if ( self.$panel.hasClass('is-open') ) return true;
         return false;
       }
 
-      // Public function
       function init() {
         // Custom select creation
         customSelectCreation();
         // Event listeners
-        $opener.on("click.customSelect", togglePanel);
-        $opener.on("keydown.customSelect", function(e) {
+        self.$opener.on("click.customSelect", togglePanel);
+        self.$opener.on("keydown.customSelect", function(e) {
           keydownPanelManager(e);
         });
-        $cstOption.on("click.customSelect", setSelectValue);
-        $cstOption.on("mouseover.customSelect", function(e) {
+        self.$cstOptions.on("click.customSelect", setSelectValue);
+        self.$cstOptions.on("mouseover.customSelect", function(e) {
           setFocus($(this));
         });
-        $select.on('change.customSelect', function(e) {
+        self.$select.on('change.customSelect', function(e) {
           updateLabelText.call($(e.currentTarget));
         });
         $(document).on("click.customSelect", outsideClosePanel);
       };
 
       init();
+  }
+
+  // Public Exposed Methods
+  CustomSelectLight.prototype = {
+    // Removes select options with the provided values: ['foo', 'buzz']
+    // If undefined triggers the empty method
+    remove: function remove(values) {
+      var self = this;
+      // check if the values are stored in an array
+      if ($.isArray(values)) {
+        values.forEach(function(value) {
+          // If the options to remove is the currently selected
+          // clears the select value and triggers the change event
+          if (self.$select.val() === value) {
+            self.$select.val('').trigger('change.customSelect');
+          }
+          // Removes the option
+          self.$select.find('option[value="'+value+'"]').remove();
+          // Removes the custom option
+          self.$cstOptions.each(function() {
+            if ($(this).attr('data-val') === value) $(this).remove();
+          });
+          // Removes the options-data used in the searchInPanel function
+          var index = self.optionsData.value.indexOf(value);
+          if (index > -1) {
+            self.optionsData.value.splice(index, 1);
+            self.optionsData.text.splice(index, 1);
+            self.optionsData.$items.splice(index, 1);
+          }
+        });
+      } else if (typeof values === 'undefined') {
+        self.empty();
+      }
+    },
+    // Clears all the options
+    // and resets all the options-data used in the searchInPanel function
+    empty: function empty() {
+      this.$select.find('option:not([value=""])').remove();
+      this.$cstOptions.remove();
+      this.$select.val('').trigger('change.customSelect');
+      this.optionsData = {
+        value: [],
+        text: [],
+        $items: []
+      };
+    },
+    // Adds new options to the select: [ ['liz', 'Liz'], ['nuts', 'Nuts'] ]
+    add: function add(elements) {
+      var self = this;
+      var optionsMarkup = "";
+      var cstOptionsMarkup = "";
+      if ($.isArray(elements)) {
+        elements.forEach(function(el) {
+          // Creates the markup string
+          optionsMarkup += '<option value="' + el[0] + '">' + el[1] + '</option>';
+          cstOptionsMarkup += '<div class="cstSelOption ' + self.options.optionClass + '" data-val="' + el[0] + '"><span>' + el[1] + '</span></div>';
+          // Populates the options-data with the new values and texts
+          self.optionsData.value.push(el[0]);
+          self.optionsData.text.push(el[1]);
+        });
+        // Appens the new DOM elements
+        self.$select.append(optionsMarkup);
+        self.$panel.append(cstOptionsMarkup);
+        // Updates the custom options used globally in the scope
+        self.optionsData.$items = self.$cstOptions = $(".cstSelOption", self.$container);
+      }
+    }
+  }
+
+  $.fn.customSelectLight = function( opt ) {
+
+    // slice arguments to leave only arguments after function name
+    var args = Array.prototype.slice.call(arguments, 1);
+    
+    return this.each( function () {
+
+      var item = $(this);
+      var instance = item.data('CustomSelectLight');
+      
+      if(!instance) {
+        // create plugin instance and save it in data
+        item.data('CustomSelectLight', new CustomSelectLight(this, opt));
+      } else {
+        // if instance already created call method
+        if(typeof opt === 'string') {
+            instance[opt].apply(instance, args);
+        }
+      }
 
     });
 
